2c717f9b87149a8cce93276b2cce6597f7f79148
JavierPalomaresGarrido <javierpalomares_97@hotmail.com>
2019-12-27 23:47:22 +0100

    MantenimientoFinal1.0

--- banco20193capas/src/main/java/edu/uclm/esi/iso2/banco20193capas/model/Cliente.java
+++ banco20193capas/src/main/java/edu/uclm/esi/iso2/banco20193capas/model/Cliente.java
@@ -1,58 +1,55 @@
 package edu.uclm.esi.iso2.banco20193capas.model;
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-/**
- * @Entity
- */
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+  @Entity
+ 
 public class Cliente {
-/**
- * @Id @GeneratedValue(strategy = GenerationType.AUTO)
- */
-/**
- * @Column(unique = true)
-*/
-	private Long id;
-	private String nif;
-	private String nombre;
-	private String apellidos;
-	/**.
-	 * sdg.
-	 */
-	public Cliente() {
-	}
-	public Cliente(final String nif, final String nombre, final String apellidos) {
-		this.nif = nif;
-		this.nombre = nombre;
-		this.apellidos = apellidos;
-	}
-	public final Long getId() {
-		return id;
-	}
-	public void setId(final Long id) {
-		this.id = id;
-	}
-	public String getNif() {
-		return nif;
-	}
-	public void setNif(final String nif) {
-		this.nif = nif;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(final String nombre) {
-		this.nombre = nombre;
-	}
-	public String getApellidos() {
-		return apellidos;
-	}
-	public void setApellidos(final String apellidos) {
-		this.apellidos = apellidos;
-	}
-	public void insert() {
-		Manager.getClienteDAO().save(this);
-	}
+
+ @Id @GeneratedValue(strategy = GenerationType.AUTO)
+ 
+
+ @Column(unique = true)
+
+  private Long id;
+  private String nif;
+  private String nombre;
+  private String apellidos;
+  public Cliente() {
+  }
+  public Cliente(final String nif, final String nombre, final String apellidos) {
+    this.nif = nif;
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+  }
+  public final Long getId() {
+    return id;
+  }
+  public void setId(final Long id) {
+    this.id = id;
+  }
+  public String getNif() {
+    return nif;
+  }
+  public void setNif(final String nif) {
+    this.nif = nif;
+  }
+  public String getNombre() {
+    return nombre;
+  }
+  public void setNombre(final String nombre) {
+    this.nombre = nombre;
+  }
+  public String getApellidos() {
+    return apellidos;
+  }
+  public void setApellidos(final String apellidos) {
+    this.apellidos = apellidos;
+  }
+  public void insert() {
+    Manager.getClienteDAO().save(this);
+  }
 }
